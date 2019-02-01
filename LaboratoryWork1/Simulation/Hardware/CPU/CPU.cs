5e4fdb496e5329a17bf27f7ba59dcb47da03a883
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-01 22:05:05 +0200

    feat(lab1): add and implement CPU class

---
+++ LaboratoryWork1/Simulation/Hardware/CPU/CPU.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Hardware
+{
+    public class CPU : ICPU {
+        public string Title { get; }
+        public int Cores { get; }
+        public double Frequency { get; }
+        public double PowerUsage { get; }
+        public double MemoryUsage { get; }
+
+        public CPU(string title, int cores, double frequency, double powerUsage, double memoryUsage) {
+            Title = title;
+            Cores = cores;
+            Frequency = frequency;
+            PowerUsage = powerUsage;
+            MemoryUsage = memoryUsage;
+        }
+    }
+}
