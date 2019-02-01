8a9392ef0907c7d615baea6c3a0731e0d918bc8f
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-01 22:27:11 +0200

    feat(lab1): add and implement OS class

---
+++ LaboratoryWork1/Simulation/Software/OS/OS.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Software
+{
+    public class OS : IOS {
+        public string Title { get; }
+        public double MemoryUsage { get; }
+
+        public OS(string title, double memoryUsage) {
+            Title = title;
+            MemoryUsage = memoryUsage;
+        }
+    }
+}
