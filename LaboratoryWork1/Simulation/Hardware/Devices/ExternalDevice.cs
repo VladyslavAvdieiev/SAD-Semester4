b77f648439faf90637268817ec859f85d1ec82e8
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-01 22:10:06 +0200

    feat(lab1): add and implement ExternalDevice class

---
+++ LaboratoryWork1/Simulation/Hardware/Devices/ExternalDevice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Hardware
+{
+    public class ExternalDevice : IExternalDevice {
+        public string Title { get; }
+        public double PowerUsage { get; }
+
+        public ExternalDevice(string title, double powerUsage){
+            Title = title;
+            PowerUsage = powerUsage;
+        }
+    }
+}
