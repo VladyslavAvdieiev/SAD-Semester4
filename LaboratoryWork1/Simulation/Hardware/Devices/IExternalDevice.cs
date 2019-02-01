e5a00cc08f89097e2516aa3464858ac4d064c1b0
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-01 22:07:27 +0200

    feat(lab1): add external device interface

---
+++ LaboratoryWork1/Simulation/Hardware/Devices/IExternalDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Hardware
+{
+    public interface IExternalDevice {
+        string Title { get; }
+        double PowerUsage { get; }
+    }
+}
