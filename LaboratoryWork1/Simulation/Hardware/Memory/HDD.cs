cf98490ea1fb506115b40975ed2323942bc9140c
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-01 18:39:16 +0200

    feat(lab1): add and implement HDD class

---
+++ LaboratoryWork1/Simulation/Hardware/Memory/HDD.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Hardware
+{
+    public class HDD : IInternalMemory {
+        private double _freeSpace;
+
+        public event EventHandler<MemoryEventArgs> OnSpaceChanged;
+        public event EventHandler<MemoryEventArgs> OnErrorOccurred;
+
+        public string Title { get; }
+        public double Capacity { get; }
+        public double FreeSpace {
+            get => _freeSpace;
+            set {
+                if (value < 0 || value > Capacity) {
+                    OnErrorOccurred?.Invoke(this, new MemoryEventArgs("There is not enough memory"));
+                    throw new OutOfMemoryException();
+                }
+                OnSpaceChanged?.Invoke(this, new MemoryEventArgs("Space has changed"));
+                _freeSpace = value;
+            }
+        }
+
+        public HDD(string title, double capacity) {
+            Title = title;
+            Capacity = capacity;
+        }
+
+        public bool Format() {
+            FreeSpace = 0d;
+            OnSpaceChanged?.Invoke(this, new MemoryEventArgs("The drive has formatted"));
+            return true;
+        }
+    }
+}
