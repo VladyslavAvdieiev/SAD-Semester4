b93d4f24ca35ffead0e4b496f210cb1fd4522390
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-01 22:00:30 +0200

    feat(lab1): add cpu interface

---
+++ LaboratoryWork1/Simulation/Hardware/CPU/ICPU.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Hardware
+{
+    public interface ICPU {
+        string Title { get; }
+        int Cores { get; }
+        double Frequency { get; }
+        double PowerUsage { get; }
+    }
+}
