7a9368efb8df97b53d099974c23fbfa01704cae5
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-01 22:29:17 +0200

    feat(lab1): add program interface

---
+++ LaboratoryWork1/Simulation/Software/Program/IProgram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Software
+{
+    public interface IProgram {
+        string Title { get; }
+        double PowerUsage { get; }
+        bool NeedsNetwork { get; }
+        double MemoryUsage { get; }
+        bool Start();
+        bool Stop();
+    }
+}
