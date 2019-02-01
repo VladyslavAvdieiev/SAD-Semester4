5eec8ab06d7d552d8c8c2372bb1e4fda3e1fdad9
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-01 18:10:59 +0200

    feat(lab1): add and implement RAM class

---
+++ LaboratoryWork1/Simulation/Hardware/Memory/MemoryEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Hardware
+{
+    public class MemoryEventArgs : EventArgs {
+        public string Message { get; }
+        public MemoryEventArgs(string message) {
+            Message = message;
+        }
+    }
+}
---
+++ LaboratoryWork1/Simulation/Hardware/Memory/RAM.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Hardware
+{
+    public class RAM : IInternalMemory {
+        private double _freeSpace;
+
+        public event EventHandler<MemoryEventArgs> OnSpaceChanged;
+        public event EventHandler<MemoryEventArgs> OnErrorOccurred;
+
+        public string Title { get; }
+        public double Capacity { get; }
+        public double FreeSpace {
+            get => _freeSpace;
+            set {
+                if (value < 0 || value > Capacity) {
+                    OnErrorOccurred?.Invoke(this, new MemoryEventArgs("There is not enough memory"));
+                    throw new OutOfMemoryException();
+                }
+                OnSpaceChanged?.Invoke(this, new MemoryEventArgs("Space has changed"));
+                _freeSpace = value;
+            }
+        }
+
+        public RAM(string title, double capacity) {
+            Title = title;
+            Capacity = capacity;
+        }
+
+        public bool Format() {
+            OnErrorOccurred?.Invoke(this, new MemoryEventArgs("Internal memory cannot be formatted"));
+            return false;
+        }
+    }
+}
