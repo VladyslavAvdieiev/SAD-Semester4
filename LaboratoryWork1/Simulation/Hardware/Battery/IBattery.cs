a66f22afdf1e5d03767711da6fd3d0c855501cad
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-01 20:55:04 +0200

    feat(lab1): add battery interface

---
+++ LaboratoryWork1/Simulation/Hardware/Battery/IBattery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Hardware
+{
+    public interface IBattery {
+        string Title { get; }
+        double Capacity { get; }
+        double FreeCharge { get; set; }
+        double Percentage { get; }
+        bool Charge();
+    }
+}
