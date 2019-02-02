3f9c3f155a6a34589dbe1d94842070826dc9f2a6
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-02 17:58:20 +0200

    feat(lab1): disable warning CS0108

--- LaboratoryWork1/Simulation/Hardware/Memory/IInternalMemory.cs
+++ LaboratoryWork1/Simulation/Hardware/Memory/IInternalMemory.cs
@@ -7,10 +7,12 @@ using System.Threading.Tasks;
 namespace Simulation.Hardware
 {
     public interface IInternalMemory : IMemory {
+        #pragma warning disable CS0108
         string Title { get; }
         double Capacity { get; }
         double FreeSpace { get; }
         double UsedSpace { get; set; }
         bool Format();
+        #pragma warning restore CS0108
     }
 }
