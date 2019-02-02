96314dc07b772f0b404c62e3e5a989159b9165c1
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-02 16:20:39 +0200

    feat(lab1): add more information about Program class

--- LaboratoryWork1/Simulation/Software/Program/Program.cs
+++ LaboratoryWork1/Simulation/Software/Program/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Simulation.Hardware;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,24 +8,43 @@ using System.Threading.Tasks;
 namespace Simulation.Software
 {
     public class Program : IProgram {
+        private IDevice _owner;
+
+        public event EventHandler<ProgramEventArgs> OnStatusChanged;
+        public event EventHandler<ProgramEventArgs> OnErrorOccurred;
+
         public string Title { get; }
         public bool NeedsNetwork { get; }
+        public bool InProgress { get; private set; }
         public double PowerUsage { get; }
         public double MemoryUsage { get; }
 
-        public Program(string title, bool needsNetwork, double powerUsage, double memoryUsage) {
+        public Program(string title, bool needsNetwork, double powerUsage, double memoryUsage, IDevice owner) {
             Title = title;
             NeedsNetwork = needsNetwork;
             PowerUsage = powerUsage;
             MemoryUsage = memoryUsage;
+            _owner = owner;
         }
 
         public bool Start() {
-            throw new NotImplementedException();
+            if (NeedsNetwork && !_owner.HasNetworkConnection) {
+                OnErrorOccurred?.Invoke(this, new ProgramEventArgs("There were troubles with network connection"));
+                return false;
+            }
+            InProgress = true;
+            OnStatusChanged?.Invoke(this, new ProgramEventArgs("The program started working"));
+            return true;
         }
 
         public bool Stop() {
-            throw new NotImplementedException();
+            if (!InProgress) {
+                OnErrorOccurred?.Invoke(this, new ProgramEventArgs("The program is not in progress"));
+                return false;
+            }
+            InProgress = false;
+            OnStatusChanged?.Invoke(this, new ProgramEventArgs("The program stopped working"));
+            return true;
         }
     }
 }
---
+++ LaboratoryWork1/Simulation/Software/Program/ProgramEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simulation.Software
+{
+    public class ProgramEventArgs : EventArgs {
+        public string Message { get; }
+        public ProgramEventArgs(string message) {
+            Message = message;
+        }
+    }
+}
