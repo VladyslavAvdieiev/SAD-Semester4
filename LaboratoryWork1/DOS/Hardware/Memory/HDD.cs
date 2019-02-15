e881b70ff3708b8b142c06ef8cea6288d05253af
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-02-15 16:37:35 +0200

    fix(lab1): fix Memoty namespace

--- LaboratoryWork1/DOS/Hardware/Memory/HDD.cs
+++ LaboratoryWork1/DOS/Hardware/Memory/HDD.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DOS.Hardware.Memory
+namespace DOS
 {
     public class HDD : IExternalStorage {
          private double _usedSpace;
