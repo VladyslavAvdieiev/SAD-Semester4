196e4f2db8d0e857026cc731172e928cccc02ae1
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-12 14:55:32 +0200

    feat(lab2): add and implement DocxType class

---
+++ LaboratoryWork2/FileCreation/DocxType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCreation
+{
+    public class DocxType : IType {
+        private string _name;
+        private const string _type = ".docx";
+
+        public DocxType(string name) {
+            _name = string.Concat(name, _type);
+        }
+
+        public string GetName() {
+            return _name;
+        }
+    }
+}
