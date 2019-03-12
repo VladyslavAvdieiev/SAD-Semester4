1d78c5d53e3559e5b7c3d73fd5525a5d111bb3e9
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-12 22:27:38 +0200

    feat(lab2): add and implement DocxFileNameCreator class

---
+++ LaboratoryWork2/FileCreation/DocxFileNameCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCreation
+{
+    public class DocxFileNameCreator : FileNameCreator {
+
+        public DocxFileNameCreator(string name) : base(name) {
+
+        }
+
+        public override IFileType CreateFileType() {
+            return new DocxType();
+        }
+    }
+}
