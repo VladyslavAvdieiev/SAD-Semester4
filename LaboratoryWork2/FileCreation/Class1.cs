5a990213644c261413ae33e2a89f0c77500aea84
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-12 13:52:31 +0200

    initialize(lab2): create class library project for business logic

--- LaboratoryWork2/FileCreation/Class1.cs
+++ LaboratoryWork2/FileCreation/Class1.cs
@@ -1,16 +1,12 @@
 ﻿using System;
-using FileCreation;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UI.ConsoleApp
+namespace FileCreation
 {
-    class Program {
-
-        static void Main(string[] args) {
-
-        }
+    public class Class1
+    {
     }
 }
