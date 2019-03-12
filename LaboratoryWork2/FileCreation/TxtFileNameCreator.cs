d78635255cefc969b6cb11eccba70c7e2ba7b6da
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-12 22:25:18 +0200

    feat(lab2): add and implement TxtFileNameCreator class

---
+++ LaboratoryWork2/FileCreation/TxtFileNameCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCreation
+{
+    public class TxtFileNameCreator : FileNameCreator {
+
+        public TxtFileNameCreator(string name) : base(name) {
+
+        }
+
+        public override IFileType CreateFileType() {
+            return new TxtType();
+        }
+    }
+}
