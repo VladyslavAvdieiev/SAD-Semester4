a1e9642f34209e9e2af7a1d6153ce5844679c877
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-12 14:56:18 +0200

    feat(lab2): add and implement JpegType class

---
+++ LaboratoryWork2/FileCreation/JpegType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCreation
+{
+    public class JpegType : IType {
+        private string _name;
+        private const string _type = ".rtf";
+
+        public JpegType(string name) {
+            _name = string.Concat(name, _type);
+        }
+
+        public string GetName() {
+            return _name;
+        }
+    }
+}
