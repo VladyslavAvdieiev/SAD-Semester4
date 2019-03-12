389c074e35dc6998bac49ba2a7ba9f022067446c
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-12 14:45:47 +0200

    feat(lab2): add and implement TxtType class

---
+++ LaboratoryWork2/FileCreation/TxtType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCreation
+{
+    public class TxtType : IType {
+        private string _name;
+        private const string _type = ".txt";
+
+        public TxtType(string name) {
+            _name = string.Concat(name, _type);
+        }
+
+        public string GetName() {
+            return _name;
+        }
+    }
+}
