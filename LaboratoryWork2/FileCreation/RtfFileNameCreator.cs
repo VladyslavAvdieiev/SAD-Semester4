f1d2f8dda9205aae7de749dfdd2a52f756db5264
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-12 22:26:32 +0200

    feat(lab2): add and implement RtfFileNameCreator class

---
+++ LaboratoryWork2/FileCreation/RtfFileNameCreator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCreation
+{
+    public class RtfFileNameCreator : FileNameCreator {
+
+        public RtfFileNameCreator(string name) : base(name) {
+
+        }
+
+        public override IFileType CreateFileType() {
+            return new RtfType();
+        }
+    }
+}
