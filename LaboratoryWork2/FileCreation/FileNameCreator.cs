6b25966efee6f4faba6839e20a70f748464945b4
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-12 22:20:39 +0200

    feat(lab2): add and implement FileNameCreator class

---
+++ LaboratoryWork2/FileCreation/FileNameCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCreation
+{
+    public abstract class FileNameCreator {
+        private readonly string _name;
+
+        public FileNameCreator(string name) {
+            _name = name;
+        }
+
+        public string GetName() {
+            IFileType fileType = CreateFileType();
+            return string.Concat(_name, fileType);
+        }
+
+        public abstract IFileType CreateFileType();
+    }
+}
