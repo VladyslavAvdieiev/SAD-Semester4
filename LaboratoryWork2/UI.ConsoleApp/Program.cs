b6d60d5e96cd76b5cd9beead60e47f12bc5deedf
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-28 15:23:01 +0200

    feat(lab2): add primitive console implementation

--- LaboratoryWork2/UI.ConsoleApp/Program.cs
+++ LaboratoryWork2/UI.ConsoleApp/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace UI.ConsoleApp
 {
@@ -11,6 +12,28 @@ namespace UI.ConsoleApp
 
         static void Main(string[] args) {
 
+            Console.Write("Enter name of file: ");
+            string name = Console.ReadLine();
+
+            string[] variants = new string[] { "txt", "jpeg", "docx", "rtf" };
+            FileNameCreator[] creators = new FileNameCreator[] { new TxtFileNameCreator(name),
+                                                                 new JpegFileNameCreator(name),
+                                                                 new DocxFileNameCreator(name),
+                                                                 new RtfFileNameCreator(name) };
+
+            int selection = SelectCreator(variants);
+
+            FileCreator.Create(creators[selection]);
+            Console.Write("File was created successfully");
+
+            Console.ReadLine();
+        }
+
+        static int SelectCreator(string[] variants) {
+            Console.WriteLine("Select type of file:");
+            for (int i = 0; i < variants.Length; i++)
+                Console.WriteLine($"{i} {variants[i]}");
+           return Convert.ToInt32(Console.ReadLine());
         }
     }
 }
