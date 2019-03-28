54f9cda3df375da4b1fac2039cca34140ffb4a26
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-28 15:11:10 +0200

    feat(lab2): add and implement FileCreator class

---
+++ LaboratoryWork2/FileCreation/File creator/FileCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileCreation
+{
+    public static class FileCreator {
+
+        public static void Create(FileNameCreator file) {
+            File.Create(file.GetName());
+        }
+    }
+}
