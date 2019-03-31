1405de8b642e6e97da54929c569b9fb5a78b325f
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-03-31 20:25:29 +0300

    feat(lab3): add primitive console implementation

---
+++ LaboratoryWork3/UI.ConsoleApp/Config.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using DOS;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.ConsoleApp
+{
+    public static class Config {
+
+        public static IEnumerable<Battery> GetBatteries() {
+            return new Battery[] {
+                new Battery(title: "Asus", capacity: 5200),
+                new Battery(title: "Samsung", capacity: 5200),
+                new Battery(title: "Lenovo", capacity: 4400),
+                new Battery(title: "HP", capacity: 2200)
+            };
+        }
+
+        public static IEnumerable<CPU> GetCPUs() {
+            return new CPU[] {
+                new CPU(title: "Intel Core I3", cores: 4, frequency: 3.6, memoryUsage: 0.3),
+                new CPU(title: "Intel Core I5", cores: 6, frequency: 2.8, memoryUsage: 0.3),
+                new CPU(title: "Intel Core I7", cores: 6, frequency: 3.2, memoryUsage: 0.5),
+                new CPU(title: "Intel Core I9", cores: 8, frequency: 3.6, memoryUsage: 1),
+            };
+        }
+
+        public static IEnumerable<RAM> GetRAMs() {
+            return new RAM[] {
+                new RAM(title: "Kingstone", capacity: 4.096),
+                new RAM(title: "HyperX", capacity: 4.096),
+                new RAM(title: "Kingstone", capacity: 8.192),
+                new RAM(title: "HyperX", capacity: 8.192),
+            };
+        }
+
+        public static IEnumerable<SSD> GetSSDs() {
+            return new SSD[] {
+                new SSD(title: "Kingstone", capacity: 120),
+                new SSD(title: "Kingstone", capacity: 240),
+                new SSD(title: "Samsung", capacity: 250),
+                new SSD(title: "Samsung", capacity: 500)
+            };
+        }
+
+        public static IEnumerable<DOS.OperatingSystem> GetOperatingSystems() {
+            return new DOS.OperatingSystem[] {
+                new DOS.OperatingSystem(title: "Windows XP", powerUsage: 5, memoryUsage: 1.2, neededStorage: 15),
+                new DOS.OperatingSystem(title: "Windows 7", powerUsage: 6, memoryUsage: 1, neededStorage: 22),
+                new DOS.OperatingSystem(title: "Windows 10", powerUsage: 7, memoryUsage: 1.5, neededStorage: 25)
+            };
+        }
+
+        public static IEnumerable<DOS.Program> GetPrograms() {
+            return new DOS.Program[] {
+                new DOS.Program(title: "GTA 5", needsNetworkConnection: true, powerUsage: 10, memoryUsage: 1, neededStorage: 62),
+                new DOS.Program(title: "Womrs", needsNetworkConnection: false, powerUsage: 2, memoryUsage: 0.5, neededStorage: 2),
+                new DOS.Program(title: "Word", needsNetworkConnection: false, powerUsage: 2, memoryUsage: 0.5, neededStorage: 8),
+                new DOS.Program(title: "Visual Studio 2017", needsNetworkConnection: false, powerUsage: 8, memoryUsage: 0.3, neededStorage: 32),
+            };
+        }
+
+        public static IEnumerable<ExternalDevice> GetExternalDevices() {
+            return new ExternalDevice[] {
+                new ExternalDevice(title: "Headphones", powerUsage: 0.2),
+                new ExternalDevice(title: "Microphone", powerUsage: 0.2),
+                new ExternalDevice(title: "Web camera", powerUsage: 0.2),
+                new ExternalDevice(title: "Printer", powerUsage: 0.2)
+            };
+        }
+    }
+}
--- LaboratoryWork3/UI.ConsoleApp/Program.cs
+++ LaboratoryWork3/UI.ConsoleApp/Program.cs
@@ -10,7 +10,115 @@ namespace UI.ConsoleApp
     class Program {
 
         static void Main(string[] args) {
+            Laptop laptop = CreateLaptop();
+            SignForEvents(laptop);
+            DisplayStatus(laptop);
 
+            Console.ReadLine();
+            laptop.TurnOn();
+
+            Console.ReadLine();
+            laptop.OperatingSystem.Programs[3].Run();
+
+            Console.ReadLine();
+            laptop.TurnOff();
+
+            Console.ReadLine();
+        }
+
+        static Laptop CreateLaptop() {
+            Battery battery = Config.GetBatteries().ElementAt(SelectComponent("Select Battery:", Config.GetBatteries()));
+            CPU cpu = Config.GetCPUs().ElementAt(SelectComponent("Select CPU:", Config.GetCPUs()));
+            RAM ram = Config.GetRAMs().ElementAt(SelectComponent("Select RAM:", Config.GetRAMs()));
+            SSD ssd = Config.GetSSDs().ElementAt(SelectComponent("Select SSD:", Config.GetSSDs()));
+            DOS.OperatingSystem os = Config.GetOperatingSystems().ElementAt(SelectComponent("Select OS:", Config.GetOperatingSystems()));
+
+            bool electricity = SelectFlag("Will new laptop have an electricity connection?[y / n]: ");
+            bool network = SelectFlag("Will new laptop have an network connection?[y / n]: ");
+
+            Console.Write("Name of laptop: ");
+            Laptop laptop = Laptop.Builder
+                                  .Battery(battery)
+                                  .Title(Console.ReadLine())
+                                  .CPU(cpu)
+                                  .RAM(ram)
+                                  .ExternalStorage(ssd)
+                                  .OperatingSystem(os)
+                                  .HasElectricityConnection(electricity)
+                                  .HasNetworkConnection(network)
+                                  .Build() as Laptop;
+
+            foreach (DOS.Program program in Config.GetPrograms())
+                laptop.OperatingSystem.Install(program);
+            return laptop;
+        }
+
+        static bool SelectFlag(string message) {
+            Console.Write(message);
+            string input = Console.ReadLine();
+            if (IsCorrect(input))
+                if (input == "y")
+                    return true;
+                else
+                    return false;
+            return SelectFlag("Wrong input. Try again: ");
+        }
+
+        static int SelectComponent<T>(string message, IEnumerable<T> array) {
+            Console.WriteLine(message);
+            Print(array);
+            string input = Console.ReadLine();
+            if (IsCorrect(input, array.Count()))
+                return int.Parse(input);
+            return SelectComponent("Wrong input. Try again:", array);
+        }
+
+        static bool IsCorrect(string input, int boundary) {
+            if (int.TryParse(input, out int output))
+                if (output >= 0 && output < boundary)
+                    return true;
+            return false;
+        }
+
+        static bool IsCorrect(string input) {
+            if (input == "y" || input == "n")
+                return true;
+            return false;
+        }
+
+        static void Print<T>(IEnumerable<T> array) {
+            for (int i = 0; i < array.Count(); i++)
+                Console.WriteLine($"{i}. {array.ElementAt(i)}");
+        }
+
+        static void SignForEvents(Laptop laptop) {
+            laptop.OnStatusChanged += (sender, e) => DisplayStatus(laptop);
+            ((DOS.OperatingSystem)laptop.OperatingSystem).OnStatusChanged += (sender, e) => DisplayStatus(laptop);
+            ((CPU)laptop.CPU).OnStatusChanged += (sender, e) => DisplayStatus(laptop);
+            ((Battery)laptop.Battery).OnChargeChanged += (sender, e) => DisplayStatus(laptop);
+            ((RAM)laptop.RAM).OnSpaceChanged += (sender, e) => DisplayStatus(laptop);
+            ((SSD)laptop.ExternalStorage).OnSpaceChanged += (sender, e) => DisplayStatus(laptop);
+            foreach (DOS.Program program in laptop.OperatingSystem.Programs)
+                program.OnStatusChanged += (sender, e) => DisplayStatus(laptop);
+        }
+
+        static void DisplayStatus(Laptop laptop) {
+            Console.Clear();
+            Console.WriteLine($"Title:              {laptop.Title}");
+            Console.WriteLine($"Device status:      {(laptop.IsEnabled ? "ON" : "OFF")}");
+            Console.WriteLine($"OS status:          {(laptop.OperatingSystem.IsEnabled ? "ON" : "OFF")}");
+            Console.WriteLine($"CPU status:         {(laptop.CPU.IsEnabled ? "ENABLED" : "DISABLED")}");
+            Console.WriteLine($"Battery charge:     {laptop.Battery.CurrentCharge} mAh");
+            Console.WriteLine($"RAM used space:     {laptop.RAM.UsedSpace} GB");
+            Console.WriteLine($"SSD used space:     {laptop.ExternalStorage.UsedSpace} GB");
+            Console.WriteLine($"Network status:     {(laptop.HasNetworkConnection ? "CONNECTED" : "DISCONNECTED")}");
+            Console.WriteLine($"Electricity status: {(laptop.HasElectricityConnection ? "CONNECTED" : "DISCONNECTED")}");
+            Console.WriteLine("Installed programs:");
+            int i = 9;
+            foreach (DOS.Program program in laptop.OperatingSystem.Programs) {
+                Console.SetCursorPosition(20, i++);
+                Console.Write($"{program.Title}: {(program.IsEnabled ? "ON" : "OFF")}");
+            }
         }
     }
 }
