9efdd53eab892327bd0e6190cd2109816e875079
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-04-05 15:00:39 +0300

    feat(lab4): implement Repository Pattern

---
+++ LaboratoryWork4/DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using DataAccessLayer.Entities;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CategoryRepository : IRepository<Category> {
+        private BoardContext db;
+
+        public CategoryRepository(BoardContext context) {
+            db = context;
+        }
+
+        public IEnumerable<Category> GetAll() {
+            return db.Categories;
+        }
+
+        public Category Get(int id) {
+            return db.Categories.Find(id);
+        }
+
+        public IEnumerable<Category> Find(Func<Category, bool> predicate) {
+            return db.Categories.Where(predicate).ToList();
+        }
+
+        public void Create(Category category) {
+            db.Categories.Add(category);
+        }
+
+        public void Update(Category category) {
+            db.Entry(category).State = EntityState.Modified;
+        }
+
+        public void Delete(int id) {
+            Category category = db.Categories.Find(id);
+            if (category != null)
+                db.Categories.Remove(category);
+        }
+    }
+}
---
+++ LaboratoryWork4/DataAccessLayer/Repositories/PostRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using DataAccessLayer.Entities;
+using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataAccessLayer.Repositories
+{
+    public class PostRepository : IRepository<Post> {
+        private BoardContext db;
+
+        public PostRepository(BoardContext context) {
+            db = context;
+        }
+
+        public IEnumerable<Post> GetAll() {
+            return db.Posts;
+        }
+
+        public Post Get(int id) {
+            return db.Posts.Find(id);
+        }
+
+        public IEnumerable<Post> Find(Func<Post, bool> predicate) {
+            return db.Posts.Where(predicate).ToList();
+        }
+
+        public void Create(Post post) {
+            db.Posts.Add(post);
+        }
+
+        public void Update(Post post) {
+            db.Entry(post).State = EntityState.Modified;
+        }
+
+        public void Delete(int id) {
+            Post post = db.Posts.Find(id);
+            if (post != null)
+                db.Posts.Remove(post);
+        }
+    }
+}
