cf5401e0f386f006ceda3c108f9d47472e09cf79
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-04-05 14:16:56 +0300

    feat(lab4): add entities

---
+++ LaboratoryWork4/DataAccessLayer/Entities/Category.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Category {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Category() {
+
+        }
+
+        public Category(string name) {
+            Name = name;
+        }
+
+        public override bool Equals(object obj) {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            if (GetHashCode() != obj.GetHashCode())
+                return false;
+            return true;
+        }
+        
+        public override int GetHashCode() {
+            return Id.GetHashCode() + Name.GetHashCode();
+        }
+
+        public override string ToString() {
+            return Name;
+        }
+    }
+}
---
+++ LaboratoryWork4/DataAccessLayer/Entities/Post.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    public class Post {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public List<Tag> Tags { get; set; }
+
+        public Post() {
+
+        }
+
+        public Post(string title) {
+            Title = title;
+        }
+
+        public override bool Equals(object obj) {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+            if (GetHashCode() != obj.GetHashCode())
+                return false;
+            return true;
+        }
+        
+        public override int GetHashCode() {
+            return Id.GetHashCode() + Title.GetHashCode() + Description.GetHashCode();
+        }
+
+        public override string ToString() {
+            return $"ID:{Id} {Title}";
+        }
+    }
+}
