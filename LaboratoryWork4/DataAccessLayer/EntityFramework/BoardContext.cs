9d3167e733f89c9c3e808173f50ad4dcdaf21cf9
Vladyslav Avdieiev <vladyslav.avd@gmail.com>
2019-04-05 14:42:02 +0300

    feat(lab4): implement EF

---
+++ LaboratoryWork4/DataAccessLayer/EntityFramework/BoardContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DataAccessLayer.Entities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class BoardContext : DbContext {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+        static BoardContext() {
+            Database.SetInitializer(new BoardDbInitializer());
+        }
+
+        public BoardContext(string connectionString) : base(connectionString) {
+
+        }
+    }
+}
